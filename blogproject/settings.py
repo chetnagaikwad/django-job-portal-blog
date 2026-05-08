90836e3372f8f1959c9f304445f2623a36df6321
Chetna Gaikwad <chetnagaikwad2005@gmail.com>
2026-05-08 14:34:58 +0530

    ready for deployment

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#h-ah8ioj43cjb@@2#^eba-ae5*!tcwt4$%c#j-s#9oi+)m-tv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
